467926bc8b489eccb0ba9dcc0be9d45df41aa28a
Gustavo Chaves <gustavoportelada@gmail.com>
2020-07-03 10:53:31 -0300

    Commit inicial

---
+++ lojong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lojong
+//
+//  Created by Gustavo Chaves on 03/07/20.
+//  Copyright © 2020 Gustavo Chaves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
