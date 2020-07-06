66e10258a6a42a2c3adf2394ce2d1582f56212e4
Gustavo Chaves <gustavoportelada@gmail.com>
2020-07-06 00:02:47 -0300

    Reorganização das pastas

--- lojong/Controllers/FundamentalsViewController.swift
+++ lojong/Controllers/FundamentalsViewController.swift
@@ -24,6 +24,7 @@ class FundamentalsViewController: UIViewController {
         
         setupConstraints()
     }
+    
     func setupNavigationBar(){
         self.title = "FUNDAMENTOS"
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Asap-Bold", size: 17)!]
